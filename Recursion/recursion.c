de8c1c4039cafaee056a2615fecb92e20b9a2902
Gauravlovescode <gqurav69@gmail.com>
2026-04-05 18:59:11 +0545

    feat: add recursive function to calculate factorial of a number

---
+++ Recursion/recursion.c
@@ -0,0 +1,22 @@
+/*WAP to find factorial of a number using recursive funtion*/
+#include <stdio.h>
+int factorial(int);
+int main()
+{
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    printf("The factorial of given number is %d", factorial(n));
+    return 0;
+}
+int factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n * factorial(n - 1));
+    }
+}
